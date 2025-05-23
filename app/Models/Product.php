d9bd18598dc1eff1ed627742790f6b5874d94b33
Milzam Hutomo <hutomo.profile@gmail.com>
2025-05-24 01:31:15 +0700

    Product UI: Added scope

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function scopeHasStock(Builder $query): void
+    {
+        $query->where('stock', '>', 0);
+    }
 }
